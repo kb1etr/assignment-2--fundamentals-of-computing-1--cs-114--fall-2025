a91ecd553527cfd5a6da94e33e1d6748617a1f03
kb1etr <schuett@hartford.edu>
2025-10-14 09:46:49 -0400

    Update PrintVowelsAndNonVowels.java

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
-   
+  String inputChar;
+  // System.out.print(inputChar.charAt(3)); 
+  Scanner keyInput = new Scanner(System.in);
+ int length(keyInput); 
+  For (int i =0 , i > length(keyInput), --i);{
+   if (inputChar.charAt(i) == i);{
+    System.out.print(inputChar.charAt(i));
+   }
+
+  }
+
 
   }
 }
