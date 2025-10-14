358ff67c4e7290b47174f2a6812ec853ec7d4843
kb1etr <schuett@hartford.edu>
2025-10-14 14:24:23 -0400

    Update PrintVowelsAndNonVowels.java

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -2,18 +2,42 @@ import java.util.Scanner;
 
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
-  String inputChar;
-  // System.out.print(inputChar.charAt(3)); 
-  Scanner keyInput = new Scanner(System.in);
-  inputChar = keyInput.nextLine();
-  int l = length(keyInput); 
-  For (int i =0 , i > l , --i);{
-   if (inputChar.charAt(i) == i);{
-    System.out.print(inputChar.charAt(i));
-   }
-
-  }
+    String input;
+    Scanner keyboardInput = new Scanner(System.in);
+    input = keyboardInput.nextLine();
+    int nonVow = input.length();
+    System.out.println(nonVow);
 
 
+    for(int i = 0; i < input.length(); i++) {
+      switch(input.charAt(i)) {
+        case 'a':
+          System.out.println("a was found");
+
+          break;
+        
+        case 'e':
+          System.out.println("e was found");
+
+          break;
+        case 'i':
+          System.out.println("i was found");
+
+          break;
+        case 'o':
+          System.out.println("o was found");
+
+          break;
+        case 'u':
+          System.out.println("u was found");
+
+          break;
+
+
+
+
+
+      }
+    }
   }
 }
