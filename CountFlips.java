f3f2afbb306b0b67332ba87ee12c82207c463314
kb1etr <schuett@hartford.edu>
2025-10-16 08:20:16 -0400

    Update CountFlips.java

---
+++ CountFlips.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class CountFlips {
+  public static void main(String[] args) {
+
+  int face = 0;
+
+   for ( int i = 0; i > 10; i++){
+    
+    face = (int) (Math.random() * 2);
+    System.out.println("Face = " + (face));
+
+    face = 0;
+
+   }
+    
+  }
+}
