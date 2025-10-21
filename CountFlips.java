21fce46527b64be98fe4e8ecb6a26fe995b30aca
kb1etr <schuett@hartford.edu>
2025-10-21 16:58:05 -0400

    Update CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,16 +1,12 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin myCoin = new Coin();
 
-  int face = 0;
+    System.out.println(myCoin.toString());
 
-   for ( int i = 0; i < 10; i++){
-    
-    face = (int) (Math.random() * 2);
-    System.out.println("Face = " + (face));
-
-    face = 0;
-
-   }
-    
+    for (int i = 0; i < 5; i++) {
+      // 1. Flip the coin
+      // 2. Print the value
+    }
   }
 }
