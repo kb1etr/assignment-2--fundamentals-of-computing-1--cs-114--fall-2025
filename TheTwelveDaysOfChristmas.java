007514ffd8e59f7b7b7e94621ceec8f6afd49eb3
kb1etr <schuett@hartford.edu>
2025-10-21 19:59:35 -0400

    Update TheTwelveDaysOfChristmas.java

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,9 +1,119 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
   
-  for (int i = 0; i < 12; i++){
+  for (int i = 1; i < 13; i++){
+
+    switch(i){
+
+    case 1:
+    System.out.println("On the 1st day of Christmas my true love gave to me");
+
+     break;
+
+   case 2:
+    System.out.println("On the 2nd day of Christmas my true love gave to me");
+
+    break;
+
+    case 3:
+     System.out.println("On the 3rd day of Christmas my true love gave to me");
+
+    break;
+
+    case 4:
+     System.out.println("On the 4th day of Christmas my true love gave to me");
+
+     break;
+
+    case 5:
+     System.out.println("On the 5th day of Christmas my true love gave to me");
+    
+    break;
+
+    case 6:
+     System.out.println("On the 6th day of Christmas my true love gave to me");
+
+     break;
+
+    case 7:
+     System.out.println("On the 7th day of Christmas my true love gave to me");
+
+    break;
+
+    case 8:
+     System.out.println("On the 8th day of Christmas my true love gave to me");
+
+    break;
+
+    case 9:
+     System.out.println("On the 9th day of Christmas my true love gave to me");
+
+    break;
+
+    case 10:
+     System.out.println("On the 10th day of Christmas my true love gave to me");
+
+    break;
+
+    case 11:
+     System.out.println("On the 11th day of Christmas my true love gave to me");
+
+    break;
+
+    case 12:
+     System.out.println("On the 12th day of Christmas my true love gave to me");
+
+    break;
+
+    }
+
+
+    switch(i){
+
+    case 12:
+    System.out.println("Twelve drummers drumming,");
+
+
+    case 11:
+    System.out.println("Eleven pipers piping,");
+
+
+    case 10:
+    System.out.println("Ten lords a-leaping,");
+
+
+    case 9:
+    System.out.println("Nine ladies dancing,");
+
+    case 8:
+    System.out.println("Eight maids a-milking,");
 
     
+    case 7:
+    System.out.println("Seven swans a-swimming,");
+
+    case 6:
+    System.out.println("Six geese a-laying,");
+
+
+    case 5:
+    System.out.println("Five golden rings,");
+
+   case 4:
+    System.out.println("Four calling birds,");
+
+   case 3:
+    System.out.println("Three French hens,");
+
+   case 2:
+    System.out.println("Two turtle doves, and");
+   
+   case 1:
+     System.out.println("A partridge in a pear tree.");
+  
+  
+  }
+
   }
   
   }
