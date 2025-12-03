c6ee612a836632ec077b5e89f24c94bdce3b4ad9
kb1etr <schuett@hartford.edu>
2025-12-03 10:09:58 -0500

    added some lines and tabs

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,6 +1,9 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
 
+    System.out.println();
+    System.out.println("The Twelve Days of Christmas\n");
+
   for (int i = 1; i < 13; i++){
 
    System.out.print("On the " + i);
@@ -30,50 +33,51 @@ public class TheTwelveDaysOfChristmas {
     }
 
     System.out.println(" day of Christmas my true love gave to me");
+    System.out.println();
 
     switch(i){
 
     case 12:
-    System.out.println("Twelve drummers drumming,");
+    System.out.println("  Twelve drummers drumming,");
 
 
     case 11:
-    System.out.println("Eleven pipers piping,");
+    System.out.println("  Eleven pipers piping,");
 
 
     case 10:
-    System.out.println("Ten lords a-leaping,");
+    System.out.println("  Ten lords a-leaping,");
 
 
     case 9:
-    System.out.println("Nine ladies dancing,");
+    System.out.println("  Nine ladies dancing,");
 
     case 8:
-    System.out.println("Eight maids a-milking,");
+    System.out.println("  Eight maids a-milking,");
 
     
     case 7:
-    System.out.println("Seven swans a-swimming,");
+    System.out.println("  Seven swans a-swimming,");
 
     case 6:
-    System.out.println("Six geese a-laying,");
+    System.out.println("  Six geese a-laying,");
 
 
     case 5:
-    System.out.println("Five golden rings,");
+    System.out.println("  Five golden rings,");
 
    case 4:
-    System.out.println("Four calling birds,");
+    System.out.println("  Four calling birds,");
 
    case 3:
-    System.out.println("Three French hens,");
+    System.out.println("  Three French hens,");
 
    case 2:
-    System.out.println("Two turtle doves, and");
+    System.out.println("  Two turtle doves, and");
    
    case 1:
-     System.out.println("A partridge in a pear tree.");
-  
+     System.out.println("  A partridge in a pear tree.");
+     System.out.println();
    }
 
   }
